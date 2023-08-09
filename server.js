85de0cc69f29e512a16bef161f3a554ec98993fa
Vivek G <audit536@gmail.com>
2023-08-09 19:13:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors({
-    origin: ['https://yakshag.github.io', 'http://localhost:3000']
+    origin: ['https://yakshag.github.io', 'http://localhost:3275']
 }));
 
 app.get('/fetch-html', (req, res) => {
