feaeeaff582a3302887e291e55248eae7534ae29
Vivek G <audit536@gmail.com>
2023-08-13 22:40:56 +0530

    Add slack on keys token generation with env file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios'); // Import the axios library
+const axios = require('axios');
+require('dotenv').config(); // Load environment variables from .env file
 
 const app = express();
 const port = 3000;
@@ -13,7 +14,6 @@ app.get('/health', async (req, res) => {
     res.status(200).send("Server is running")
 });
 
-
 app.get('/fetch-html', async (req, res) => {
     const { url } = req.query;
 
@@ -22,10 +22,45 @@ app.get('/fetch-html', async (req, res) => {
     }
 
     try {
-        const response = await axios.get(url, { maxRedirects: 5 }); // Set maximum number of redirects
+        const response = await axios.get(url, { maxRedirects: 5 });
         res.send(response.data);
     } catch (error) {
         res.status(500).json({ error: 'Error fetching HTML' });
+    }
+});
+
+app.get("/slack-on-keys/:code", async (req, res) => {
+    const tempCode = req.params.code;
+    const oauthUrl = "https://slack.com/api/oauth.v2.access";
+    const analyticsUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=425970658&text=slack-on-keys,%20new-slack-token-generated&parse_mode=Markdown`;
+
+    const oauthData = new URLSearchParams();
+    oauthData.append('client_id', process.env.SLACK_CLIENT_ID);
+    oauthData.append('client_secret', process.env.SLACK_CLIENT_SECRET);
+    oauthData.append('code', tempCode);
+
+    try {
+        const oauthResponse = await axios.post(oauthUrl, oauthData, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        });
+
+        if (oauthResponse.data.ok === true) {
+            const token = oauthResponse.data.authed_user.access_token;
+            res.status(200).send(token);
+        } else {
+            res.status(400).send(oauthResponse.data.error);
+        }
+    } catch (error) {
+        res.status(500).send('Error during OAuth request');
+    }
+
+    try {
+        await axios.post(analyticsUrl);
+        console.log('Anonymous analytics');
+    } catch (error) {
+        console.error('Error sending analytics:', error);
     }
 });
 
